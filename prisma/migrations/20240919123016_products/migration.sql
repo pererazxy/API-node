479c0456afc332dceea1b4afc792cf113b91888d
pererazxy <ph287807@gmail.com>
2024-09-19 10:03:24 -0300

    first commit

---
+++ prisma/migrations/20240919123016_products/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "Stock" INTEGER NOT NULL
+);
